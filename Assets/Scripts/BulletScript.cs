34b5e188220fad3eb1292c20a8efe4e8859a8f05
Nassim Amghar <nassimamg@hotmail.com>
2020-01-26 15:22:18 +0100

    first commit

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    public float speed = 15.0f;
+
+    void FixedUpdate()
+    {
+        // Get the horizontal and vertical axis.
+        // By default they are mapped to the arrow keys.
+        // The value is in the range -1 to 1
+        float translation =  speed;
+
+        // Make it move 10 meters per second instead of 10 meters per frame...
+        translation *= Time.deltaTime;
+
+        // Move translation along the object's z-axis
+        transform.Translate(0, 0, translation);
+
+    }
+}
